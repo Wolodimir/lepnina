c55d41332a9fa4dd61fae8447b7e663c8edfd9bf
Wolodimir <dvdent666@gmail.com>
2021-02-04 19:22:33 +0400

    Second test deploy

--- src/main/java/ru/lepnina/shop/cart/CartController.java
+++ src/main/java/ru/lepnina/shop/cart/CartController.java
@@ -1,8 +1,6 @@
 package ru.lepnina.shop.cart;
 
 import org.springframework.web.bind.annotation.*;
-import ru.lepnina.shop.client.ClientService;
-import ru.lepnina.shop.product.ProductService;
 
 
 @RestController
@@ -10,13 +8,9 @@ import ru.lepnina.shop.product.ProductService;
 @RequestMapping("/api/cart")
 public class CartController {
 
-    private final ClientService clientService;
-    private final ProductService productService;
     private final CartService cartService;
 
-    public CartController(ClientService clientService, ProductService productService, CartService cartService) {
-        this.clientService = clientService;
-        this.productService = productService;
+    public CartController(CartService cartService) {
         this.cartService = cartService;
     }
 
